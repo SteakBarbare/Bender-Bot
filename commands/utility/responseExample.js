8fee7517b2ae498da890d2d1a5be713f5ec5cbf2
Steak Barbare <cortodufour@gmail.com>
2024-02-27 02:21:09 +0100

    Update: Options, Permissions & Buttons

--- commands/utility/responseExample.js
+++ commands/utility/responseExample.js
@@ -6,12 +6,12 @@ module.exports = {
     .setName("bender_response")
     .setDescription("Gives a simple reply, but has all types in comments"),
   async execute(interaction) {
-    // // await interaction.deferReply();
-    // // await wait(2_000);
+    // await interaction.deferReply();
+    // await wait(2_000);
     await interaction.reply("Saucisse");
     // await interaction.followUp("Deuxième Saucisse");
-    // // await interaction.editReply("Saucisse éditée");
-    // // await interaction.reply({ content: "Saucisse privée", ephemeral: true });
+    // await interaction.editReply("Saucisse éditée");
+    // await interaction.reply({ content: "Saucisse privée", ephemeral: true });
     // const message = await interaction.fetchReply();
     // console.log(message);
   }
